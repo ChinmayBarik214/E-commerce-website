c506ac33f2f1541eaec1e6f43dfe1703865e2028
Chinmay <chinmaybarik214@gmail.com>
2023-10-26 15:37:40 +0530

    Finished Logo and Search Bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCilsA12tdHqZGXaYVnkni2LC7O7UVIC64",
+    authDomain: "clone-144d5.firebaseapp.com",
+    projectId: "clone-144d5",
+    storageBucket: "clone-144d5.appspot.com",
+    messagingSenderId: "867553648152",
+    appId: "1:867553648152:web:5ae55fdc7dffc667062d6d",
+    measurementId: "G-516VWD5K41"
+};
