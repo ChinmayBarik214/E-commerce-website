e1807afe631778c996b52aa21ae269cae87e20ac
Chinmay <chinmaybarik214@gmail.com>
2023-10-28 15:02:44 +0530

    Added all products

--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import  "./Product.css"
 
-function Product() {
+function Product({id, title, image, price, rating, alt}) {
   return (
     <div className="product">
         <div className="product__info">
-            <p>The lean startup</p>
+            <p>{title}</p>
             <p className="product__price">
-                <small>₹</small>
-                <strong>505</strong>
+              <small>₹</small>
+              <strong>{price}</strong>
             </p>
             <div className="product__rating">
+              {Array(rating)
+                .fill()
+                .map((_, i) => (
                 <p>⭐</p>
+              ))}
             </div>
         </div>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="The Lean Startup cover"/>
+        <img src={image} alt={alt}/>
         <button>Add to Basket</button>
     </div>
   )
