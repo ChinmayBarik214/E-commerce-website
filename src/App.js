5d3476a2ef1bae8ef214468028c931d011192f3b
Chinmay <chinmaybarik214@gmail.com>
2023-10-28 16:44:27 +0530

    Added react-router-dom functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from "./Home";
-
+import {BrowserRouter,
+        Routes,
+        Route
+} from "react-router-dom";
 
 function App() {
   return (
     // BEM
+    <BrowserRouter>
     <div className="app">
-      <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={
+        <div>
+        <Header />
+        <Home />
+        </div>
+        }/>
+        <Route path="checkout/*" element={
+        <div>
+        <Header />
+        <h1>This is the checkout page</h1>
+        </div>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
